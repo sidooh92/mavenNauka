8e143f700675bc8ec142ff3639f9e1728ecb377d
robert <robert.piotrowski92@gmail.com>
2018-07-07 09:42:43 +0200

    collection

--- src/main/java/our/mycollection/MyCollectionImpl.java
+++ src/main/java/our/mycollection/MyCollectionImpl.java
@@ -4,8 +4,19 @@ package our.mycollection;
 public class MyCollectionImpl implements MyCollection {
 
 
+    private MyNode firstElement;
+    private MyNode lastElement;
+    private int size = 0;
+
     @Override
     public void enqueue(MyNode myNode) {
+        //TODO: JEZELI KOLEJKA JEST PUSTA TO
+        // FIRST I LAST WSKAZUJA
+        // NA TEN SAM NOWT ELEMENT
+
+        //TODO: WIECEJ ELEMENTOW
+        //PIERWSZY ZOSTAJE BEZ ZMIAN, DRUGI JEST OSTATNIM
+
 
     }
 
