3054ed2b6e675083d2a6ebbc55e56f6a3a5c0c97
robert <robert.piotrowski92@gmail.com>
2018-07-08 11:30:57 +0200

    tree

--- src/main/java/our/mymap/Main.java
+++ src/main/java/our/mymap/Main.java
@@ -12,18 +12,15 @@ public class Main {
         map.put("Maj", asciValueOfMonth("Maj"));
         map.put("Czerwiec", asciValueOfMonth("Czerwiec"));
         map.put("Lipiec", asciValueOfMonth("Lipiec"));
-        //log.info("{}",map.get("Maj"));
-        //nastpenie wyswietl zawartosc mapy uzywajac iteratora
-        //wykorzystac entrySet i iterator hasNext i map.entry
-                         map.entrySet();
-        Set set = map.entrySet();
-        Iterator iterator = set.iterator(); //mapa nie implementuje
-        while(iterator.hasNext()) {
-            Map.Entry ent = (Map.Entry) iterator.next();
-            log.info("{}",ent);
+//        Set set = map.entrySet();
+//        Iterator iterator = set.iterator(); //mapa nie implementuje
+//        while(iterator.hasNext()) {
+//            Map.Entry ent = (Map.Entry) iterator.next();
+//            log.info("{}",ent);
+//        }
+        log.info("{}",map.containsKey("Grudzien"));
+        log.info("{}",map.get("Maj"));
         }
-
-    }
 
     private static Integer asciValueOfMonth(String month) {
         int sum = 0;
