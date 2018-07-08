3beb046f285b499772192990598463643c9b7e2d
robert <robert.piotrowski92@gmail.com>
2018-07-08 11:11:16 +0200

    tree

--- src/main/java/our/mymap/Main.java
+++ src/main/java/our/mymap/Main.java
@@ -2,23 +2,34 @@ package our.mymap;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-      Map<String,Integer> map = new HashMap<>();
-      //string to key, interger to value
-        map.put("Maj",asciValueOfMonth("Maj"));
-        //wypelnij tablice miesiacami nadaja im wartosc
-        // bedaca suma ich liter w ascii
-
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Luty", asciValueOfMonth("Luty"));
+        map.put("Maj", asciValueOfMonth("Maj"));
+        map.put("Czerwiec", asciValueOfMonth("Czerwiec"));
+        map.put("Lipiec", asciValueOfMonth("Lipiec"));
+        //log.info("{}",map.get("Maj"));
         //nastpenie wyswietl zawartosc mapy uzywajac iteratora
-        //wykorzystac entrySet i iterator hasNext
+        //wykorzystac entrySet i iterator hasNext i map.entry
 
+        Set set = map.entrySet();
+        Iterator iterator = set.iterator(); //mapa nie implementuje
+        while(iterator.hasNext()) {
+            Map.Entry ent = (Map.Entry) iterator.next();
+            log.info("{}",ent);
+        }
 
+    }
 
+    private static Integer asciValueOfMonth(String month) {
+        int sum = 0;
+        byte[] ascii = month.getBytes();
+        for (byte anAscii : ascii) sum += anAscii;
+        return sum;
     }
 
 }
