25a0f313c2759778a357418863c8183cb2ed283b
robert <robert.piotrowski92@gmail.com>
2018-08-04 10:44:08 +0200

    init 4/8/2018 tdd

--- src/test/java/prime/PrimeNumberTest.java
+++ src/test/java/prime/PrimeNumberTest.java
@@ -13,15 +13,64 @@ public class PrimeNumberTest {
     private static Logger log =
             LoggerFactory.getLogger(PrimeNumberTest.class);
 
+    //TODO napisac logike
+    //TODO refaktor
+    //TODO podpiac junit params
+    //TODO refaktor na test z parametrami
+
+    @Test
+    void shouldCheckIf1IsPrimeNumber() {
+        //given
+        int number =1;
+        PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
+        //when
+        boolean isPrime = primeNumberUtils.isPrime(number);
+        //then
+        assertThat(isPrime).isFalse();
+    }
+
+    @Test
+    void shouldCheckIf2IsPrimeNumber() {
+        //given
+        int number =2;
+        PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
+        //when
+        boolean isPrime = primeNumberUtils.isPrime(number);
+        //then
+        assertThat(isPrime).isTrue();
+    }
+
+
+    @Test
+    void shouldCheckIf3IsPrimeNumber() {
+        //given
+        int number =3;
+        PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
+        //when
+        boolean isPrime = primeNumberUtils.isPrime(number);
+        //then
+        assertThat(isPrime).isTrue();
+    }
+
+
+    @Test
+    void shouldCheckIf4IsPrimeNumber() {
+        //given
+        int number =4;
+        PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
+        //when
+        boolean isPrime = primeNumberUtils.isPrime(number);
+        //then
+        assertThat(isPrime).isFalse();
+    }
+
     @Test
     void shouldCheckIf5IsPrimeNumber() {
         //given
         int number =5;
         PrimeNumberUtils primeNumberUtils = new PrimeNumberUtils();
-
         //when
         boolean isPrime = primeNumberUtils.isPrime(number);
-
         //then
         assertThat(isPrime).isTrue();
     }
